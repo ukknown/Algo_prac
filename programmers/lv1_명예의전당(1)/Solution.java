df117ebbf9e2484a5ad5e11a406f608c95d6ab1e
Jonguk Choi <87683992+ukknown@users.noreply.github.com>
2023-10-09 18:11:57 +0900

    Create Solution.java

---
+++ programmers/lv1_명예의전당(1)/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < score.length; i++){
+            if(pq.size() < k){
+                pq.add(score[i]);
+                answer[i] = pq.peek();
+            }else{
+                if(score[i] > pq.peek()){
+                    pq.add(score[i]);
+                    pq.poll();
+                }
+            }
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
